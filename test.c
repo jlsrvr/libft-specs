3092c235ba09ac311564f886f4907961bd63f0c7
Jules Rivoire <julesriv@gmail.com>
2020-11-02 18:41:03 +0100

    Add tests for ft_isdigit

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 
 int ft_strlen(char *str);
 int ft_isalpha(int c);
+int ft_isdigit(int c);
 char *write_boolean(int value);
 
 int main(int ac, char **av)
@@ -15,6 +16,32 @@ int main(int ac, char **av)
 
 	(void)ac;
 	(void)av;
+
+	// -----------------------FT_ISDIGIT--------------------------
+
+	printf("\nTests for ft_isdigit\n");
+
+	char *chars = "J e\n/:0123456789";
+
+	for(index = 0; chars[index]; index++)
+	{
+		expected = isdigit(chars[index]);
+		result = ft_isdigit(chars[index]);
+		if (expected == result)
+		{
+			printf("\033[1;32m");
+			printf("OK for char = \"%c\"\n", chars[index]);
+			printf("\033[0m");
+		}
+		else
+		{
+			printf("\033[0;31m--------------\n");
+			printf("KO! for char = \"%c\"\n\n", chars[index]);
+			printf("\033[1;32mExpected = %s\n", write_boolean(expected));
+			printf("\033[0;31mGot = %s\n\n",  write_boolean(result));
+			printf("\033[0m");
+		}
+	}
 
 	// -----------------------FT_ISALPHA--------------------------
 
