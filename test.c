4a824d8c61ed4aefd7ddc5271c8cac9137b605cf
Jules Rivoire <julesriv@gmail.com>
2020-11-02 20:08:38 +0100

    Add tests for ft_isascii

--- test.c
+++ test.c
@@ -7,6 +7,7 @@ int ft_strlen(char *str);
 int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalnum(int c);
+int ft_isascii(int c);
 char *write_boolean(int value);
 
 int main(int ac, char **av)
@@ -20,6 +21,35 @@ int main(int ac, char **av)
 
 	(void)ac;
 	(void)av;
+
+	// -----------------------FT_ISASCII--------------------------
+
+	printf("\nTests for ft_isascii\n");
+
+	succes = 0;
+	failure = 0;
+	for(index = -2; index < 135; index++)
+	{
+		expected = isascii(index);
+		result = ft_isascii(index);
+		if ((expected && result) || (!expected && !result))
+		{
+			succes++;
+			//printf("\033[1;32m");
+			//printf("OK for char = \"%c\"\n", index);
+			//printf("\033[0m");
+		}
+		else
+		{
+			failure++;
+			printf("--------------\n");
+			printf("\033[0;31mKO! for ascii char = \"%d\"\n\n", index);
+			printf("\033[1;32mExpected = %s\n", write_boolean(expected));
+			printf("\033[0;31mGot = %s\n\n",  write_boolean(result));
+			printf("\033[0m");
+		}
+	}
+	printf("\t%d success out of %d tests\n", succes, (succes + failure));
 
 	// -----------------------FT_ISALNUM--------------------------
 
