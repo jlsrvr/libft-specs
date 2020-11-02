7cca574a3ffc1c50465af6c12f6dc3ed5169cb35
Jules Rivoire <julesriv@gmail.com>
2020-11-02 18:08:50 +0100

    Add tests for ft_isalpha

--- test.c
+++ test.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int ft_strlen(char *str);
-
+int ft_isalpha(int c);
+char *write_boolean(int value);
 
 int main(int ac, char **av)
 {
 	int index;
+	int expected;
+	int result;
 
 	(void)ac;
 	(void)av;
 
+	// -----------------------FT_ISALPHA--------------------------
+
+	printf("\nTests for ft_isalpha\n");
+
+	chars = "J1 e\nAaZz@[`{";
+
+	for(index = 0; chars[index]; index++)
+	{
+		expected = isalpha(chars[index]);
+		result = ft_isalpha(chars[index]);
+		if (expected == result)
+		{
+			printf("\033[1;32m");
+			printf("OK for char = \"%c\"\n", chars[index]);
+			printf("\033[0m");
+		}
+		else
+		{
+			printf("\033[0;31m--------------\n");
+			printf("KO! for char = \"%c\"\n\n", chars[index]);
+			printf("\033[1;32mExpected = %s\n", write_boolean(expected));
+			printf("\033[0;31mGot = %s\n\n",  write_boolean(result));
+			printf("\033[0m");
+		}
+	}
+
 	// -----------------------FT_STRLEN--------------------------
 
-	printf("Tests ft_strlen\n");
+	printf("\nTests for ft_strlen\n");
 
 	char strs[5][15] = {"Jean", "", " ", "adore vraiment", "\n"} ;
-	int expected;
-	int result;
 
 	for(index = 0; index < 5; index++)
 	{
@@ -42,4 +70,11 @@ int main(int ac, char **av)
 
 
 	return (0);
+}
+
+char *write_boolean(int value)
+{
+	if (value == 0)
+		return ("False");
+	return ("True");
 }
