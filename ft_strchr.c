42ec77eb6d48675ebb79abd07cd7aa37eceb4bc9
Jules Rivoire <julesriv@gmail.com>
2020-11-07 11:56:05 +0100

    Add ft_strchr function (/!\ include string.h)

---
+++ ft_strchr.c
@@ -0,0 +1,16 @@
+//HEADER
+
+#include <stdio.h>
+
+char *ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char*)s);
+		s++;
+	}
+		if (*s == c)
+			return ((char*)s);
+	return(NULL);
+}
