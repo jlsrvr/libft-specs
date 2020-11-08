92523acadc33d045101af1c3b8cbabf00b4ad88c
Jules Rivoire <julesriv@gmail.com>
2020-11-08 18:18:11 +0100

    Add ft_strncmp (/!\ include string.h)

---
+++ ft_strncmp.c
@@ -0,0 +1,17 @@
+//HEADER
+
+#include <string.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t index;
+
+	index = 0;
+	while ((s1[index] || s2[index]) && index < n)
+	{
+		if (s1[index] != s2[index])
+			return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+		++index;
+	}
+	return (0);
+}
