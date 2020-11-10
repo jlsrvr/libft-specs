3d73e58c5f98ce40fb69499f7974f91119d4ca6c
Jules Rivoire <julesriv@gmail.com>
2020-11-10 19:41:06 +0000

    Add ft_strlcat function (/!\ include string.h et proto ft_strlen)

---
+++ ft_strlcat.c
@@ -0,0 +1,24 @@
+//HEADER
+
+#include <string.h>
+int ft_strlen(char *str);
+
+size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+	size_t lendst;
+	size_t lensrc;
+	size_t index;
+
+	lendst = (ft_strlen((char*)dst));
+	lensrc = (ft_strlen((char*)src));
+	index = 0;
+	if (lendst >= dstsize)
+		return (dstsize + lensrc);
+	while ((lendst + index) < dstsize - 1 && src[index])
+	{
+		dst[(lendst + index)] = src[index];
+		++index;
+	}
+	dst[(lendst + index)] = '\0';
+	return (lendst + lensrc);
+}
