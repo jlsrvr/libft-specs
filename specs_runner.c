311aadabb9259a3f7dcae5d5da63d93a015593bb
Jules Rivoire <julesriv@gmail.com>
2020-11-15 13:57:27 +0000

    Refacto specs_runner for selective testing

--- specs_runner.c
+++ specs_runner.c
@@ -1,8 +1,24 @@
 void spec_first_part(void);
+void spec_second_part(void);
+
+int ft_atoi(char *str);
+void (*parts[3])(void);
 
 int main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
-	spec_first_part();
+	parts[0] = spec_first_part;
+	parts[1] = spec_second_part;
+	parts[2] = 0;
+
+	int selected;
+
+	if (ac > 1)
+	{
+		selected = ( ft_atoi(av[1]) - 1);
+		(*parts[selected])();
+		return (0);
+	}
+	for (selected = 0; parts[selected]; selected++)
+		(*parts[selected])();
+	return (0);
 }
