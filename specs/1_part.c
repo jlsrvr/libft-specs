d04cb6ecdeee9a7948eece4c86608c1269aa6648
Jules Rivoire <julesriv@gmail.com>
2020-11-15 15:07:18 +0000

    use libft.h in specs for part one

--- specs/1_part.c
+++ specs/1_part.c
@@ -1,34 +1,9 @@
+#include "../libft.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <strings.h>
 #include <ctype.h>
 #include <malloc/malloc.h>
 
-void		*ft_memset(void *ptr, int value, size_t num);
-void		ft_bzero(void *ptr, size_t num);
-void		*ft_memcpy(void *dest, const void *src, size_t n);
-void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
-void		*ft_memmove(void *dest, const void *src, size_t len);
-void		*ft_memchr(const void *s, int c, size_t n);
-int			ft_memcmp(const void *s1, const void *s2, size_t n);
-char		*ft_strchr(const char *s, int c);
-char		*ft_strrchr(const char *s, int c);
-int			ft_strncmp(const void *s1, const void *s2, size_t n);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
-int			ft_atoi(const char *str);
-char		*ft_strdup(const char *src);
-void		*ft_calloc(size_t count, size_t size);
-int			ft_strlen(char *str);
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
-int			ft_isalnum(int c);
-int			ft_isascii(int c);
-int			ft_isprint(int c);
-int			ft_toupper(int c);
-int			ft_tolower(int c);
 char		*write_boolean(int value);
 void		print_null_begun_string(void *str, int size);
 int			comp_null_begun_strings(void *str1, void *str2, int size);
@@ -49,7 +24,7 @@ void spec_first_part(void)
 
 	// -----------------------FT_CALLOC--------------------------
 
-	printf("\nTests for ft_strdup\n");
+	printf("\nTests for ft_calloc\n");
 	succes = 0;
 	failure = 0;
 
