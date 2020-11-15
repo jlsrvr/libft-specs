8c3745e4c89ec6669a98c62c3f5dff2097f4b17f
Jules Rivoire <julesriv@gmail.com>
2020-11-15 14:32:54 +0000

    libft v1

---
+++ libft.h
@@ -0,0 +1,32 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(const char *src);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char		*ft_strrchr(const char *s, int c);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+int			ft_atoi(const char *str);
+int			ft_isalnum(int c);
+int			ft_isalpha(int c);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+int			ft_strlen(char *str);
+int			ft_strncmp(const void *s1, const void *s2, size_t n);
+int			ft_tolower(int c);
+int			ft_toupper(int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void		ft_bzero(void *ptr, size_t num);
+void		*ft_calloc(size_t count, size_t size);
+void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t len);
+void		*ft_memset(void *ptr, int value, size_t num);
+
+#endif
