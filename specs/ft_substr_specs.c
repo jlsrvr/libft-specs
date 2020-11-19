db14a572838fa1d642856d44e3c98001c9ddf22b
Jules Rivoire <julesriv@gmail.com>
2020-11-19 17:08:01 +0000

    Improve tests for ft_substr
    protect from segfault by guard clause if null
    test when start is greater than len of s

--- specs/ft_substr_specs.c
+++ specs/ft_substr_specs.c
@@ -15,80 +15,95 @@ void display_result(char *original, char *new, int start, size_t len, int succes
 
 void specs_ft_substr(int *succes, int *failure)
 {
-	int index;
+	int start;
 	size_t len;
 	char strs[4][14] = {"adore vraiment", "", " ", "\n"} ;
 	char *ptn_result;
-	for(index = 0; strs[0][index]; index++)
+	for(start = 0; strs[0][start]; start++)
 	{
-		ptn_result = ft_substr(strs[0], index, 6);
-		if ((ft_strlen(ptn_result) <= 6) && !ft_strncmp(&strs[0][index], ptn_result, 6))
+		ptn_result = ft_substr(strs[0], start, 6);
+		if ((ft_strlen(ptn_result) <= 6) && !ft_strncmp(&strs[0][start], ptn_result, 6))
 		{
 			(*succes)++;
-			//display_result(strs[0], ptn_result, index, 6, 1);
+			//display_result(strs[0], ptn_result, start, 6, 1);
 		}
 		else
 		{
 			(*failure)++;
-			display_result(strs[0], ptn_result, index, 6, 0);
+			display_result(strs[0], ptn_result, start, 6, 0);
 		}
 		free(ptn_result);
 	}
-	index = 1;
+	start = 1;
 	len = 6;
-	ptn_result = ft_substr(strs[1], index, len);
-	if ((ft_strlen(ptn_result) <= len) && !ft_strncmp(&strs[1][index], ptn_result, len))
+	ptn_result = ft_substr(NULL, start, len);
+	if ((ft_strlen(ptn_result) <= len) && (ptn_result == NULL))
 	{
 		(*succes)++;
-		//display_result(strs[1], ptn_result, index, len, 1);
+		//display_result(strs[1], ptn_result, start, len, 1);
 	}
 	else
 	{
 		(*failure)++;
-		display_result(strs[1], ptn_result, index, 6, 0);
+		display_result(strs[1], ptn_result, start, 6, 0);
 	}
 	free(ptn_result);
-	index = 3;
+	start = 3;
 	len = 0;
-	ptn_result = ft_substr(strs[0], index, len);
-	if ((ft_strlen(ptn_result) <= len) && !ft_strncmp(&strs[0][index], ptn_result, len))
+	ptn_result = ft_substr(strs[0], start, len);
+	if ((ft_strlen(ptn_result) <= len) && !ft_strncmp(&strs[0][start], ptn_result, len))
 	{
 		(*succes)++;
-		//display_result(strs[0], ptn_result, index, len, 1);
+		//display_result(strs[0], ptn_result, start, len, 1);
 	}
 	else
 	{
 		(*failure)++;
-		display_result(strs[0], ptn_result, index, len, 0);
+		display_result(strs[0], ptn_result, start, len, 0);
 	}
 	free(ptn_result);
-	index = 0;
+	start = 0;
 	len = 6;
-	ptn_result = ft_substr(strs[2], index, len);
-	if ((ft_strlen(ptn_result) <= len) && !ft_strncmp(&strs[2][index], ptn_result, len))
+	ptn_result = ft_substr(strs[2], start, len);
+	if ((ft_strlen(ptn_result) <= len) && !ft_strncmp(&strs[2][start], ptn_result, len))
 	{
 		(*succes)++;
-		//display_result(strs[2], ptn_result, index, len, 1);
+		//display_result(strs[2], ptn_result, start, len, 1);
 	}
 	else
 	{
 		(*failure)++;
-		display_result(strs[2], ptn_result, index, len, 0);
+		display_result(strs[2], ptn_result, start, len, 0);
 	}
 	free(ptn_result);
-	index = 15;
+	start = 15;
 	len = 30;
 	char *str = "\n\tqwertyuioooasdfghjkzxcvbnm123456@£$%";
-	ptn_result = ft_substr(str, index, len);
-	if ((ft_strlen(ptn_result) <= len) && !ft_strncmp(&str[index], ptn_result, len))
+	ptn_result = ft_substr(str, start, len);
+	if ((ft_strlen(ptn_result) <= len) && !ft_strncmp(&str[start], ptn_result, len))
 	{
 		(*succes)++;
-		//display_result(str, ptn_result, index, len, 1);
+		//display_result(str, ptn_result, start, len, 1);
 	}
 	else
 	{
 		(*failure)++;
-		display_result(str, ptn_result, index, len, 0);
+		display_result(str, ptn_result, start, len, 0);
+	}
+	free(ptn_result);
+	start = 15;
+	len = 30;
+	char *s = "une string";
+	ptn_result = ft_substr(s, start, len);
+	if ((ft_strlen(ptn_result) <= len) && !ft_strncmp("", ptn_result, len))
+	{
+		(*succes)++;
+		//display_result(s, ptn_result, start, len, 1);
+	}
+	else
+	{
+		(*failure)++;
+		display_result(s, ptn_result, start, len, 0);
 	}
 	free(ptn_result);
 }
