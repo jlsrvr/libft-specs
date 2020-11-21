c3cf1e76c186eee91d93f733d860d33fc62661d1
Jules Rivoire <julesriv@gmail.com>
2020-11-21 16:58:12 +0000

    add libft and deal with NULL strings in strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,27 +1,35 @@
-//HEADER
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/21 16:59:14 by jrivoire          #+#    #+#             */
+/*   Updated: 2020/11/23 20:05:30 by jrivoire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-int ft_strlen(char *str);
-
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int index;
-	char *dest;
+	int		index;
+	char	*dest;
 
-	if (!(dest = malloc(sizeof(*dest) * (ft_strlen((char*)s1) + ft_strlen((char*)s2) + 1))))
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	if (!(dest = malloc(sizeof(*dest) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (NULL);
 	index = 0;
 	while (*s1)
-	{
-		dest[index++] = *s1;
-		s1++;
-	}
+		dest[index++] = *(s1++);
 	while (*s2)
-	{
-		dest[index++] = *s2;
-		s2++;
-	}
+		dest[index++] = *(s2++);
 	dest[index] = 0;
 	return (dest);
 }
