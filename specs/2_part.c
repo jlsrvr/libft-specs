ac7e872f1403be2d95833924fc7dce9802c2f7bf
Jules Rivoire <julesriv@gmail.com>
2020-11-29 12:45:38 +0000

    Add "tests" for ft_putnbr_fd

--- specs/2_part.c
+++ specs/2_part.c
@@ -9,6 +9,7 @@ void specs_ft_strmapi(int *success, int *failure);
 void specs_ft_putchar_fd(int *success, int *failure);
 void specs_ft_putstr_fd(int *success, int *failure);
 void specs_ft_putendl_fd(int *success, int *failure);
+void specs_ft_putnbr_fd(int *success, int *failure);
 
 int		success;
 int		failure;
@@ -26,7 +27,7 @@ void print_results(int success, int failure)
 
 void spec_second_part(void)
 {
-	void (*specs[10])(int*, int*);
+	void (*specs[11])(int*, int*);
 	int selected;
 
 	success = 0;
@@ -41,7 +42,8 @@ void spec_second_part(void)
 	specs[6] = specs_ft_putchar_fd;
 	specs[7] = specs_ft_putstr_fd;
 	specs[8] = specs_ft_putendl_fd;
-	specs[9] = 0;
+	specs[9] = specs_ft_putnbr_fd;
+	specs[10] = 0;
 
 	for (selected = 0; specs[selected]; selected++)
 	{
---
+++ specs/ft_putnbr_fd_specs.c
@@ -0,0 +1,24 @@
+#include "../libft.h"
+#include <stdio.h>
+
+void specs_ft_putnbr_fd(int *sucess, int *failure)
+{
+	printf("Tests for %s\n", &__func__[6]);
+	(void)sucess;
+	(void)failure;
+	int index;
+	int ints[5];
+	ints[3] = 2147483647;
+	ints[0] = -2147483648;
+	ints[1] = -5;
+	ints[2] = 1;
+	ints[4] = 0;
+
+	for(index = 0; ints[index]; index++)
+	{
+		ft_putnbr_fd(ints[index], 1);
+		printf("==");
+		printf("%d", ints[index]);
+		printf("\n");
+	}
+}
