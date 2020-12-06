86de0ae178302085add567b314c8ff1183aeea64
Jules Rivoire <julesriv@gmail.com>
2020-12-06 21:16:09 +0100

    Adding guard clause to ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/07 23:10:01 by jrivoire          #+#    #+#             */
+/*   Updated: 2020/12/07 23:10:03 by jrivoire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -5,10 +16,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t index;
 
+	if (n == 0)
+		return (dest);
 	index = 0;
 	while (index < n)
 	{
-		((char*)dest)[index] = ((char*)src)[index];
+		((char*)dest)[index] = ((const char*)src)[index];
 		index++;
 	}
 	return (dest);
