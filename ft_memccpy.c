4ac5260687b765d3502d0e90dffe2c3c641136c0
Jules Rivoire <julesriv@gmail.com>
2020-12-07 23:11:11 +0100

    Add hearder and clean up to ft_memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,15 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/07 23:09:08 by jrivoire          #+#    #+#             */
+/*   Updated: 2020/12/07 23:09:43 by jrivoire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	size_t index;
+	size_t	index;
+	char	*csrc;
+	char	*cdest;
 
+	csrc = (char*)src;
+	cdest = (char*)dest;
 	index = 0;
 	while (index < n)
 	{
-		((char*)dest)[index] = ((char*)src)[index];
-		if (((char*)src)[index] == c)
+		cdest[index] = csrc[index];
+		if ((unsigned char)(csrc[index]) == (unsigned char)c)
 			return (&dest[index + 1]);
 		index++;
 	}
