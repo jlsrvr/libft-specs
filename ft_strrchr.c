264f4151183bce3ac1a28b3a30d94539ef2a8306
Jules Rivoire <julesriv@gmail.com>
2020-12-10 21:07:15 +0100

    Make it so that ft_strrchr checks the first letter of the string

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 23:11:49 by jrivoire          #+#    #+#             */
-/*   Updated: 2020/11/30 23:12:00 by jrivoire         ###   ########.fr       */
+/*   Updated: 2020/12/10 21:04:23 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t index;
+	int index;
 
-	index = ft_strlen((char*)s);
-	while (index > 0)
+	index = (int)ft_strlen((char*)s);
+	while (index >= 0)
 	{
 		if (s[index] == c)
 			return ((char*)(s + index));
