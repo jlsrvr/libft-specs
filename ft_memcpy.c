2072bae6fec76283c303520b46e870447fd72ff3
Jules Rivoire <julesriv@gmail.com>
2020-12-12 16:05:02 +0100

    Protect memcpy from NULL pointers in both strings

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 23:10:01 by jrivoire          #+#    #+#             */
-/*   Updated: 2020/12/07 23:10:03 by jrivoire         ###   ########.fr       */
+/*   Updated: 2020/12/12 16:04:16 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t index;
+	size_t		index;
+	char		*pdest;
+	const char	*psrc;
 
-	if (n == 0)
-		return (dest);
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	index = 0;
+	pdest = dest;
+	psrc = src;
 	while (index < n)
 	{
-		((char*)dest)[index] = ((const char*)src)[index];
+		*pdest++ = *psrc++;
 		index++;
 	}
 	return (dest);
