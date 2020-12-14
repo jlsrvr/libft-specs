767d126be4ad7f542322efa41b6646c50bf9db0d
Jules Rivoire <julesriv@gmail.com>
2020-12-14 23:01:16 +0100

    Dealing with the case where dest and src null in memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 20:57:28 by jrivoire          #+#    #+#             */
-/*   Updated: 2020/12/10 20:35:31 by jrivoire         ###   ########.fr       */
+/*   Updated: 2020/12/14 22:52:34 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 
 	csrc = (char*)src;
 	cdest = (char*)dest;
+	if (!dest && !src)
+		return (NULL);
 	while ((int)--len >= 0)
 		if (csrc < cdest)
 			*(cdest + len) = *(csrc + len);
