faa7ee9a838f3e8b5fd8cd48e511e2a26d7b475c
Jules Rivoire <julesriv@gmail.com>
2020-12-14 22:01:00 +0100

    Add memset 0 after have allocated memory space in ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 20:54:16 by jrivoire          #+#    #+#             */
-/*   Updated: 2020/11/30 20:54:47 by jrivoire         ###   ########.fr       */
+/*   Updated: 2020/12/14 21:42:28 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	*ft_calloc(size_t count, size_t size)
 	void *dest;
 
 	if ((dest = malloc(size * count)))
-		return (dest);
+		return (ft_memset(dest, 0, count));
 	return (NULL);
 }
