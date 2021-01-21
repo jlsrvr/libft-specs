74aed67adfc23c06ee02ae55642469da9bf5cc9c
Jules Rivoire <julesriv@gmail.com>
2021-01-21 14:28:30 +0100

    Normé

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,13 +6,13 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 19:40:01 by jrivoire          #+#    #+#             */
-/*   Updated: 2020/12/28 20:13:40 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/01/21 14:27:45 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst || !del)
 		return ;
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 18:26:22 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/01/04 22:50:11 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/01/21 14:27:10 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,15 @@ typedef struct	s_list
 {
 	void			*content;
 	struct s_list	*next;
-}	t_list;
+}				t_list;
 
 char			*ft_itoa(int n);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strdup(const char *src);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char			*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char			*ft_strnstr(const char *haystack,
+					const char *needle, size_t len);
 char			*ft_strtrim(char const *s1, char const *set);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
@@ -51,7 +52,8 @@ size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t			ft_strlen(char const *str);
 
 t_list			*ft_lstlast(t_list *lst);
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 t_list			*ft_lstnew(void *content);
 
 void			ft_bzero(void *ptr, size_t num);
