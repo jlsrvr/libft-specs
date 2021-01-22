294687e0c2c502d459f0dc036ed01eb9592e1edb
Jules Rivoire <julesriv@gmail.com>
2021-01-22 13:20:17 +0100

    Remove stdio from lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,12 +6,11 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/17 22:44:26 by jrivoire          #+#    #+#             */
-/*   Updated: 2020/12/18 00:07:21 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/01/22 13:18:36 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_lstsize(t_list *lst)
 {
